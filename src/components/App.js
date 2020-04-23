ff4abd1c868b5e13653c845bcf84607e037957fd
Faye <si.fi.faye.li@gmail.com>
2020-04-23 18:41:04 +0800

    progress

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { selectSong } from '../actions';
+
+const App = () => {
+    return <div>App</div>;
+};
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+const songsReducer = () => {
+    return [
+        { title: 'No Scrubs', duration: '4:05' },
+        { title: 'Macarena', duration: '2:30' },
+        { title: 'I Want it That Way', duration: '1:45'},
+        { title: 'All Start', duration: '3:15'},
+    ];
+};
